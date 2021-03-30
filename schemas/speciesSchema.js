dcc173bafd29def1048d67ad9ee771a4afff49d4
maris <mari.seppanen4@metropolia.fi>
2021-03-30 18:08:28 +0300

    initial

---
+++ schemas/speciesSchema.js
@@ -0,0 +1,13 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+  type Species {
+    id: ID
+    speciesName: String
+    category: Category
+  }
+
+  extend type Mutation {
+    addSpecies(speciesName: String!, category: ID!): Species
+  }
+`;
